b944bc694ab176bb5f20fc98f0f9c66c5b70f536
Calum Cuthill <calum.cuthill@gmail.com>
2022-05-24 11:04:18 +0100

    soil moisture and battery monitor

--- iot_sensor.py
+++ iot_sensor.py
@@ -2,6 +2,9 @@ import time
 import alarm
 from circuitpy_mcu.mcu import Mcu
 import adafruit_htu31d
+import analogio
+import board
+from adafruit_lc709203f import LC709203F, PackSize
 
 # scheduling and event/error handling libs
 from watchdog import WatchDogTimeout
@@ -11,13 +14,13 @@ import adafruit_logging as logging
 import traceback
 
 __version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/calcut/circuitpy-septic_tank"
-__filename__ = "septic_tank.py"
+# __repo__ = "https://github.com/calcut/circuitpy-septic_tank"
+# __filename__ = "septic_tank.py"
 
 # Set AIO = True to use Wifi and Adafruit IO connection
 # secrets.py file needs to be setup appropriately
-AIO = True
-# AIO = False
+# AIO = True
+AIO = False
 
 def main():
 
@@ -62,7 +65,9 @@ def main():
     # mcu.attach_sdcard()
     # mcu.archive_file('log.txt')
     # mcu.archive_file('data.txt')
-
+    soil_sensor = analogio.AnalogIn(board.A0)
+    battery_monitor = LC709203F(mcu.i2c)
+    battery_monitor.pack_size = PackSize.MAH1000
 
     if AIO:
         mcu.wifi_connect()
@@ -83,11 +88,18 @@ def main():
         if mcu.aio_connected:
             feeds['greenhouse.temperature'] = round(htu1.temperature, 2)
             feeds['greenhouse.humidity'] = round(htu1.relative_humidity, 2)
+            feeds['greenhouse.soil'] = 100* soil_sensor.value // 65536
             feeds['outside.temperature'] = round(htu2.temperature, 2)
             feeds['outside.humidity'] = round(htu2.relative_humidity, 2)
             mcu.aio_send(feeds)
 
-    mcu.watchdog.feed()
+
+    while True:
+        print(f'{battery_monitor.cell_percent=}')
+        print(f'{battery_monitor.cell_voltage=}')
+        print(f'{100* soil_sensor.value // 65536}')
+        time.sleep(1)
+        mcu.watchdog.feed()
     mcu.aio_receive()
 
     publish_feeds()
